9e68a85f7778e6dbbd16c3a1beea6d336b83aad8
asepsofyan <asep.sofyan@bening-semesta.com>
2024-10-01 13:28:56 +0700

    match case

---
+++ 2_withif.py
@@ -0,0 +1,21 @@
+command = input("masukan perintah:(hello, [hello, asep], [jumlahkan, angka]): ")
+inputs = command.split(' ')
+
+match inputs:
+
+    case ["hello"]:
+        name = input("nama anda: ")
+        print("hello ", name)
+
+    case ["hello", "asep"]:
+        print("hello asep, apa kabar?")
+
+    case ["jumlahkan","angka"]:
+        numbers = input("masukan urutan angka integer, pisahkan dengan spasi :")
+        total = 0
+        for str in numbers.split(' '):
+            total = total + int(str)
+        print("total:", total)
+
+    case _:
+        print("perintah tidak diketahui:")
